e328d82dbd5cdcd408d1ebccfd7e811c121fd37d
hzs90 <zsolt.huszar@live.com>
2018-12-12 22:24:41 +0100

    first version

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import Navbar from "../components/Navbar";
+import Layout from "../components/Layout";
+
+const About = () => (
+  <Layout>
+    {" "}
+    <div>
+      <h1>About BitzPrice</h1>
+      <p>Application to view bitcoin prices</p>
+    </div>
+  </Layout>
+);
+
+export default About;
